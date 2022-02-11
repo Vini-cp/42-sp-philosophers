c858ab0051320c139ed25082fd0ae559b00f99e4
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2022-02-11 11:44:51 -0300

    Create project structure

---
+++ philo/include/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 11:34:16 by vcordeir          #+#    #+#             */
+/*   Updated: 2022/02/11 11:37:51 by vcordeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#endif
