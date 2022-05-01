1514f042f00b39168bfe6b63ce016e6ef9f70ae5
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-01 18:13:51 +0200

    Define main file

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 11:33:26 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/04/25 01:36:11 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/01 18:13:34 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 int	main(int argc, char **argv)
 {
-	t_philo	philo;
+	t_philo			philo;
+	t_philosopher	*philosophers;
 
+	philosophers = NULL;
 	if (ft_initiate_constraints(argc, argv, &philo) != E_SUCCESS)
 		return (0);
-	if (ft_initiate_philosophers(&philo) != E_SUCCESS)
+	if (ft_initiate_forks(&philo) != E_SUCCESS)
 		return (0);
-	ft_run_philosophers();
-	ft_free(&philo);
+	if (ft_initiate_philosophers(&philo, &philosophers) != E_SUCCESS)
+		return (0);
+	// ft_print_philosophers(philo.number_of_philosophers, philosophers);
+	ft_run_philosophers(philosophers);
+	free(philo.forks);
+	free(philosophers);
 	return (0);
 }
