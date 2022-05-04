46e23bd12d0645fd4f1c5f2798c5b71813274899
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-05 01:45:42 +0200

    Remove time parameter

--- philo/src/utils/ft_print_action.c
+++ philo/src/utils/ft_print_action.c
@@ -6,17 +6,15 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 01:01:02 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/04/25 01:19:12 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/05 00:44:17 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-void	ft_print_action(long long action_time,
-						int action_id,
-						int philosopher_id)
+void	ft_print_action(int action_id, int philosopher_id)
 {
-	printf("%lld %d ", action_time, philosopher_id);
+	printf("%lld %d ", ft_get_time_now(), philosopher_id);
 	if (action_id == E_TAKE_A_FORK)
 		printf("has taken a fork\n");
 	else if (action_id == E_EAT)
