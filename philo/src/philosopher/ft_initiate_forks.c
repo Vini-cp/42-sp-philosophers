2895c618a99cb3a88190b6800a41b95e490242b0
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-05 01:41:35 +0200

    Add a meal checker mutex

--- philo/src/philosopher/ft_initiate_forks.c
+++ philo/src/philosopher/ft_initiate_forks.c
@@ -6,13 +6,13 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 20:49:25 by coder             #+#    #+#             */
-/*   Updated: 2022/05/01 03:33:07 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/03 02:31:31 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-static e_error	create_forks(int size, pthread_mutex_t **forks)
+static e_error	ft_create_forks(int size, pthread_mutex_t **forks)
 {
 	int	i;
 
@@ -29,11 +29,19 @@ static e_error	create_forks(int size, pthread_mutex_t **forks)
 	return (E_SUCCESS);
 }
 
+static e_error	ft_create_eat_checker(t_philo *philo)
+{
+	if (pthread_mutex_init(&(philo->finished_eating), NULL))
+		return (E_INSTATIATE_MUTEX);
+	return (E_SUCCESS);
+}
+
 e_error	ft_initiate_forks(t_philo *philo)
 {
 	e_error	error_no;
 
-	error_no = create_forks(philo->number_of_philosophers, &philo->forks);
+	error_no = ft_create_forks(philo->number_of_philosophers, &philo->forks);
+	error_no = ft_create_eat_checker(philo);
 	if (error_no != E_SUCCESS)
 	{
 		if (philo->forks)
