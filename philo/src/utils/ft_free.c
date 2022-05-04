216b631d0931a46e48c0d34476e4c37621326071
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-05 01:47:04 +0200

    Change function return type

--- philo/src/utils/ft_free.c
+++ philo/src/utils/ft_free.c
@@ -6,14 +6,15 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 19:22:42 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/04/24 23:09:11 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/05 00:48:16 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-void	ft_free(t_philo *philo)
+int	ft_free(t_philo *philo, t_philosopher **philosophers, int error_code)
 {
 	free(philo->forks);
-	free(philo->philosophers);
+	free(*philosophers);
+	return (ft_print_error(error_code));
 }
