b5da2150ed050077ce286b6d6a04247904cdb46f
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-06 04:10:11 +0200

    Fix print action

--- philo/src/utils/ft_print_action.c
+++ philo/src/utils/ft_print_action.c
@@ -6,26 +6,22 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 01:01:02 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/05/05 02:10:41 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/06 04:05:52 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-void	ft_print_action(int action_id, int philosopher_id, long long start_time)
+void	ft_print_action(int philosopher_id, int action_id, long long start_time)
 {
 	long long	time_to_print;
-
+	char	*action[E_NBR_OF_ACTIONS];
+	
+	action[E_TAKE_A_FORK] = "has taken a fork\n";
+	action[E_EAT] = "is eating\n";
+	action[E_SLEEP] = "is sleeping\n";
+	action[E_THINK] = "is thinking\n";
+	action[E_DIE] = "died\n";
 	time_to_print = ft_time_diff(start_time, ft_get_time_now());
-	printf("%lld %d ", time_to_print, philosopher_id);
-	if (action_id == E_TAKE_A_FORK)
-		printf("has taken a fork\n");
-	else if (action_id == E_EAT)
-		printf("is eating\n");
-	else if (action_id == E_SLEEP)
-		printf("is sleeping\n");
-	else if (action_id == E_THINK)
-		printf("is thinking\n");
-	else if (action_id == E_DIE)
-		printf("died\n");
+	printf("%.4lld %d %s", time_to_print, philosopher_id, action[action_id]);
 }
