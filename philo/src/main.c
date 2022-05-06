f159eb60c9a210864ef5ca54dda925399356e795
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-06 04:13:23 +0200

    Construct project flow

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 11:33:26 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/05/05 00:48:59 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/06 03:32:21 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,24 @@ int	main(int argc, char **argv)
 	t_philo			philo;
 	t_philosopher	*philosophers;
 
-	philosophers = NULL;
+	philosophers = NULL;	
 	if (ft_initiate_constraints(argc, argv, &philo) != E_SUCCESS)
 		return (0);
 	if (ft_initiate_forks(&philo) != E_SUCCESS)
 		return (0);
 	if (ft_initiate_philosophers(&philo, &philosophers) != E_SUCCESS)
 		return (0);
-	if (ft_run_philosophers(philosophers) != E_SUCCESS)
-		return (0);
+	philo.start_time = ft_get_time_now();
+	i = 0;
 	while (i < philo.number_of_philosophers)
 	{
+		philosophers[i].last_meal_was_at = philo.start_time;
 		if (pthread_create(&(philosophers[i].thread_id), NULL, ft_run_philosophers, &(philosophers[i])))
 			return (ft_free(&philo, &philosophers, E_THREAD_CREATION));
-		philosophers[i].last_meal_was_at = ft_get_time_now();
 		i++;
 	}
+	// ft_print_philosophers(philo.number_of_philosophers, philosophers);
+	ft_philosopher_has_died(&philosophers);
 	ft_exit_threads(philosophers);
 	return (ft_free(&philo, &philosophers, E_SUCCESS));
 }
