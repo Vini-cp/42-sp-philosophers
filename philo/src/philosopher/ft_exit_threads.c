a4361e7c18940f3597cfb0c0acd6992f8d3196f1
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-07 23:15:16 +0200

    Join threads before destroy mutexes

--- philo/src/philosopher/ft_exit_threads.c
+++ philo/src/philosopher/ft_exit_threads.c
@@ -6,24 +6,27 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 01:21:41 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/05/05 01:23:49 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/07 22:55:10 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-void	ft_exit_threads(t_philosopher *philosophers)
+void	ft_exit_threads(t_philo *philo, t_philosopher **philosophers)
 {
-	int		i;
-	t_philo	*philo;
+	int	i;
 
 	i = 0;
-	philo = philosophers->philo;
-	pthread_mutex_destroy(&(philo->finished_eating));
 	while (i < philo->number_of_philosophers)
 	{
-		pthread_join(philosophers[i].thread_id, NULL);
-		pthread_mutex_destroy(&(philosophers[i].last_meal_checker));
+		pthread_join((*philosophers)[i].thread_id, NULL);
+		i++;
+	}
+	pthread_mutex_destroy(&(philo->finished_eating));
+	i = 0;
+	while (i < philo->number_of_philosophers)
+	{
+		pthread_mutex_destroy(&((*philosophers)[i].last_meal_checker));
 		pthread_mutex_destroy(&(philo->forks[i]));
 		i++;
 	}
