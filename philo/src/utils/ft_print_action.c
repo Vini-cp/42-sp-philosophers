7e7c90004b413ad17baa785719c64c14e566d322
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-07 18:27:54 +0200

    Check has_died before printing

--- philo/src/utils/ft_print_action.c
+++ philo/src/utils/ft_print_action.c
@@ -6,13 +6,13 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 01:01:02 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/05/06 04:05:52 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/07 17:56:59 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-void	ft_print_action(int philosopher_id, int action_id, long long start_time)
+void	ft_print_action(int philosopher_id, int action_id, t_philo *philo)
 {
 	long long	time_to_print;
 	char	*action[E_NBR_OF_ACTIONS];
@@ -22,6 +22,7 @@ void	ft_print_action(int philosopher_id, int action_id, long long start_time)
 	action[E_SLEEP] = "is sleeping\n";
 	action[E_THINK] = "is thinking\n";
 	action[E_DIE] = "died\n";
-	time_to_print = ft_time_diff(start_time, ft_get_time_now());
-	printf("%.4lld %d %s", time_to_print, philosopher_id, action[action_id]);
+	time_to_print = ft_time_diff(philo->start_time, ft_get_time_now());
+	if (!philo->has_died)
+		printf("%.4lld philo_%d %s", time_to_print, philosopher_id, action[action_id]);
 }
